554ed459b5dee01582044697fd4c398ec6fbb699
Amit Yadav <88823371+Tyrant670@users.noreply.github.com>
2022-09-07 16:49:12 +0530

    Update Coin Piles.cpp

--- Introductory Problems/Coin Piles.cpp
+++ Introductory Problems/Coin Piles.cpp
@@ -1,1 +1,27 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define int ll
+// const int x= 1e9+7;
+ 
+ 
+int32_t main(){
+    int t; cin>>t;
+    while(t--){
+        int a,b;cin>>a>>b;
+ 
+        int s=a+b;
+ 
+        
+ 
+        if(s%3!=0 || (a>2*b || b>2*a)){
+            cout<<"NO"<<endl;
+            continue;
+        }
+ 
+ 
+        cout<<"YES"<<endl;
+    }
+ 
+    return 0;
+}
